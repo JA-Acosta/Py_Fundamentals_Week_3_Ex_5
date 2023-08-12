13f35785fd001f7ddb98b37c620fd885344213a9
JAAR <joe.aaron.acosta@outlook.com>
2023-08-11 22:47:34 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,16 @@
+'''
+>>> JAAR
+>>> 08/11/2023
+>>> Practicing Fundamentals Program 17
+>>> Version 0
+'''
+
+'''
+>>> Creates a program that takes a file and copies all of it's contents to another file. The user then signs the file.
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
